eef3dbbd812c8cf407c891daeeea1aa5d0132ed7
tekihei2317 <tekihei2317@gmail.com>
2021-06-13 16:46:31 +0900

    feat: 問題を1問追加

---
+++ app/master_data/problem_002/initialize.sql
@@ -0,0 +1,9 @@
+create table master_numbers_1 (
+  id integer unsigned primary key auto_increment,
+  num integer not null
+);
+
+create table master_numbers_2 (
+  id integer unsigned primary key auto_increment,
+  num integer not null
+);
