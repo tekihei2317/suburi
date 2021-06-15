6dd8adafc18fd4dd037d4c48d5e04ad5cb3ef374
tekihei2317 <tekihei2317@gmail.com>
2021-06-15 20:16:13 +0900

    feat: 判定スクリプトを問題が複数ある場合に対応

--- app/scripts/judge_query.rb
+++ app/scripts/judge_query.rb
@@ -1,12 +1,14 @@
 require "mysql2"
 require_relative "utils/db_client.rb"
+require_relative "utils/problem.rb"
 
-def judge_solution(prefix, users_solution)
-  client = Mysql2::Client.new(host: "db", password: "password")
-  client.query("use problem_1")
+def first_query(file_path)
+  File.read(file_path).split(";").first
+end
 
-  answer = client.query("select * from #{prefix}_answers")
-  judge_result = client.query(users_solution)
+def judge_solution(db_client, prefix, solution_path)
+  answer = db_client.execute("select * from #{prefix}_answers")
+  judge_result = db_client.execute(first_query(solution_path))
 
   puts "result: #{answer.to_a.join(" ")}"
   puts "expected: #{judge_result.to_a.join(" ")}"
@@ -15,24 +17,29 @@ def judge_solution(prefix, users_solution)
   return answer.to_a == judge_result.to_a
 end
 
+# TODO: 引数が足りない場合を考慮する
+problem_name, solution_path = ARGV
+problem = Problem.new(problem_name)
+
 db_client = DbClient.new(host: "db", password: "password")
+db_name = problem_name
+db_client.execute("use #{db_name}")
 
-prefixes = ["01", "02", "03"]
-tables = ["pairs"]
-
-solution = readlines.map(&:strip).join(" ")
-
-prefixes.each do |prefix|
+problem.test_prefixes.each do |prefix|
   # クエリを実行するため、テーブル名を変更する
-  tables.each do |table_name|
-    db_client.rename_table("#{prefix}_#{table_name}", table_name)
+  problem.tables.each do |table_name|
+    db_client.rename_table(db_name, "#{prefix}_#{table_name}", table_name)
   end
 
-  is_correct = judge_solution(prefix, solution)
-  puts is_correct ? "AC" : "WA"
-
-  # テーブル名をもとに戻す
-  tables.each do |table_name|
-    db_client.rename_table(table_name, "#{prefix}_#{table_name}")
+  begin
+    is_correct = judge_solution(db_client, prefix, solution_path)
+    puts is_correct ? "AC" : "WA"
+  rescue => e
+    puts e.message
+  ensure
+    # テーブル名をもとに戻す
+    problem.tables.each do |table_name|
+      db_client.rename_table(db_name, table_name, "#{prefix}_#{table_name}")
+    end
   end
 end
---
+++ app/scripts/test.rb
@@ -0,0 +1,3 @@
+a, b = gets.split(" ")
+p a, b
+p File.read("./master_data/problem_001/01_pairs.sql")
